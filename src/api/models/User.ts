9e7662b592702cbe21b7a279e3b5f3a953716f09
Eric Heimerman <eric@heimerman.org>
2020-12-21 22:49:04 -0600

    working through unit tests, making progress

--- src/api/models/User.ts
+++ src/api/models/User.ts
@@ -1,13 +1,22 @@
-import {Column, Entity } from "typeorm";
+import {BeforeInsert, BeforeUpdate, Column, Entity, Unique } from "typeorm";
 import { EntityBase } from "../../utils/EntityBase"
+import bcrypt from 'bcrypt'
 
 @Entity()
 export class User extends EntityBase
 {
-    @Column({nullable: false})
-    password: string;
+    @Column({ nullable: false, name: 'password' })
+    private _password: string;
 
-    @Column({nullable: false})
+    get password(): string {
+        return this._password;
+    }
+
+    set password(password: string) {
+        this._password = bcrypt.hashSync(password, 10);
+    }
+
+    @Column({ nullable: false, unique: true })
     email: string;
 
     @Column({nullable: false})
@@ -18,6 +27,17 @@ export class User extends EntityBase
 
     @Column({nullable: true})
     lastSeenAt: Date;
+
+    // @BeforeInsert()
+    // encryptPassword() {
+    //     this.password = bcrypt.hashSync(this.password,new Date().getTime().toString());
+    // }
+
+    // @BeforeUpdate()
+    // encryptPasswordIfChanged() {
+    //     let orig = 
+
+    // }
 
     //one-to-many and many-to-many associations
 }
--- src/tests/models/user.ts
+++ src/tests/models/user.ts
@@ -1,4 +1,4 @@
-import faker from 'faker'
+import faker, { date } from 'faker'
 
 import DB, {getRepos, getRepo} from '../../utils/db'
 
@@ -12,26 +12,21 @@ beforeAll(async () => {
     userRepo = getRepos().userRepo;
 })
 
-afterAll(async () => {
-    //await DB.getInstance().close()
-})
-
 describe('save', () => {
     it('should create user', async () => {
         const email = faker.internet.email()
         const password = faker.internet.password()
         const name = faker.name.firstName()
         const before = Date.now()
-
-        const user = await userRepo.save({ email: email, password: password, name: name})
-
-        console.log({user})
+        const user = userRepo.create({ email: email, name: name})
+        user.password = password;
+        await userRepo.save(user);
 
         const after = Date.now()
 
-        const fetched = await userRepo.findOne(user.id) as User
+        console.log({before, save: user.createdAt.getTime(), after})
 
-        console.log({fetched})
+        const fetched = await userRepo.findOne(user.id) as User
 
         expect(fetched).not.toBeNull()
 
@@ -39,62 +34,76 @@ describe('save', () => {
         expect(fetched!.name).toBe(name)
         expect(fetched!.password).not.toBe(password)
 
+        //TODO: these fail because mysql is truncating milliseconds in its dates
         expect(before).toBeLessThanOrEqual(fetched!.createdAt.getTime())
         expect(fetched!.createdAt.getTime()).toBeLessThanOrEqual(after)
     })
 
     it('should update user', async () => {
         const name1 = faker.name.firstName()
-        const user = { email: faker.internet.email(), password: faker.internet.password(), name: name1 }
-        const dbUser1 = await userRepo.create(user);
+        const user = userRepo.create({ email: faker.internet.email(), name: name1 })
+        user.password = faker.internet.password()
+        const dbUser1 = await userRepo.save(user);
 
         const name2 = faker.name.firstName()
         dbUser1.name = name2
         await userRepo.update(dbUser1.id,dbUser1)
         const dbUser2 = await userRepo.findOne(dbUser1.id);
 
-        await setTimeout(()=>{}, 1000);
         expect(dbUser2!.name).toEqual(name2)
     })
 
+    //TODO: need a validator for this one
     it('should not save user with invalid email', async () => {
         const user1 = { email: 'email@em.o', password: faker.internet.password() }
-        await expect(userRepo.create(user1)).rejects.toThrowError(/invalid email address/)
+        await expect(userRepo.save(user1)).rejects.toThrowError(/invalid email address/)
     })
 
     it('should not save user without an email', async () => {
-        const user = { password: faker.internet.password(), name: faker.name.firstName() }
-        await expect(userRepo.create()).rejects.toThrowError(/email/)
+        const user = userRepo.create({ name: faker.name.firstName() })
+        user.password = faker.internet.password()
+
+        await expect(userRepo.save(user)).rejects.toThrowError(/email/)
     })
 
     it('should not save user without a password', async () => {
         const user2 = { email: faker.internet.email(), name: faker.name.firstName() }
-        await expect(userRepo.create()).rejects.toThrowError(/password/)
+        await expect(userRepo.save(user2)).rejects.toThrowError(/password/)
     })
 
     it('should not save user without a name', async () => {
-        const user1 = { email: faker.internet.email(), password: faker.internet.password() }
-        await expect(userRepo.create()).rejects.toThrowError(/name/)
+        const user1 = userRepo.create({ email: faker.internet.email()})
+        user1.password = faker.internet.password()
+        await expect(userRepo.save(user1)).rejects.toThrowError(/name/)
     })
 
     it('should not save user with the same email', async () => {
         const email = faker.internet.email()
         const password = faker.internet.password()
         const name = faker.name.firstName()
-        const userData = { email: email, password: password, name: name }
+        const userData = {email: email, name: name}
 
-        const user1 = await userRepo.create(userData)
+        const user1 = userRepo.create(userData)
+        user1.password = password
+        await userRepo.save(user1)
 
-        await expect(userRepo.create(userData)).rejects.toThrowError(/E11000/)
+        const user2 = userRepo.create(userData)
+        user2.password = password
+
+        await expect(userRepo.save(user2)).rejects.toThrowError(/UNIQUE constraint/)
     })
 
     it('should not save password in a readable form', async () => {
         const password = faker.internet.password()
 
-        const user1 = await userRepo.create({ email: faker.internet.email(), password: password, name: faker.name.firstName() })
+        const user1 = userRepo.create({ email: faker.internet.email(), name: faker.name.firstName() })
+        user1.password = password
+        await userRepo.save(user1)
         expect(user1.password).not.toBe(password)
 
-        const user2 = await userRepo.create({ email: faker.internet.email(), password: password, name: faker.name.firstName() })
+        const user2 = userRepo.create({ email: faker.internet.email(), name: faker.name.firstName() })
+        user2.password = password
+        await userRepo.save(user2)
         expect(user2.password).not.toBe(password)
 
         expect(user1.password).not.toBe(user2.password)
