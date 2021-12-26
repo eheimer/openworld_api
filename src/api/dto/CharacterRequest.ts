395808818e3fde6f3309458e29152d980e93b7cb
Eric Heimerman <eric@heimerman.org>
2021-12-26 13:10:08 -0600

    fixed bug in character creation

--- src/api/dto/CharacterRequest.ts
+++ src/api/dto/CharacterRequest.ts
@@ -3,9 +3,10 @@ import { Entity } from 'typeorm'
 @Entity()
 export class CharacterRequest {
   constructor(args: any) {
-    const { name, maxHp, inventorySize } = args
+    const { name, maxHp, baseResist, inventorySize } = args
     this.name = name
     this.maxHp = maxHp
+    this.baseResist = baseResist
     this.inventorySize = inventorySize
   }
   name: string
