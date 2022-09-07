8aab893b885dc42431e13090f391ba133b5930be
Eric Heimerman <eric@heimerman.org>
2022-09-07 15:18:28 -0500

    change up config to default to dev values if no environment is specified

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,41 +1,40 @@
-import { DataSource } from 'typeorm'
-
+//defaults are based on dev environment
+//this is so that if other environments are not
+//set up correctly, we won't overwrite any data
+//if dev gets overwritten, we don't care
 const configDefault = {
+  type: 'sqlite',
+  database: 'dev.sqlite',
   synchronize: false,
+  logging: 'warn',
+  entities: ['**/*.entity.js'],
   migrations: [`migrations/*.js`],
+  subscribers: ['subscribers/*.js'],
   cli: {
     migationsDir: 'migrations'
-  },
-  subscribers: ['subscribers/*.js']
+  }
 }
 
 // these env values override the defaults
 const dbConfig = {
   test: {
-    type: 'sqlite',
     database: 'test.sqlite',
-    logging: 'warn',
     entities: [`**/*.entity.ts`]
   },
-  dev: {
-    type: 'sqlite',
-    database: 'dev.sqlite',
-    logging: 'debug',
-    entities: [`**/*.entity.js`]
-  },
+  dev: {},
   prod: {
     type: 'mysql',
     server: 'localhost',
     port: '3306',
     username: 'openworld',
     password: 'entranced',
-    database: 'openworld',
-    logging: 'all',
-    entities: [`**/*.entity.js`]
+    database: 'openworld'
   }
 }
 
-const env: string = process.env.NODE_ENV || 'development'
+// again, default to 'dev' if an environment is not specified
+// to prevent production data from being overwritten
+const env: string = process.env.NODE_ENV || 'dev'
 const config = dbConfig[env]
 
 if (!config) {
