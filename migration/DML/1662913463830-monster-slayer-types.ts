bb1f7874805114d955a6a8a01e701849e18fda15
Eric Heimerman <eric@heimerman.org>
2022-09-11 12:44:32 -0500

    improved slayer type matching

--- migration/DML/1662913463830-monster-slayer-types.ts
+++ migration/DML/1662913463830-monster-slayer-types.ts
@@ -9,24 +9,32 @@ export class monsterSlayerTypes1662913463830 implements MigrationInterface {
     for (const monster of monsters) {
       let matchingSlayerTypes: SlayerType[] = []
       // add the monster-specific slayer if it exists
-      const monsterSlayerType = slayerTypes.find((st) => st.name === `${monster.name} Slayer`)
+      const monsterSlayerType = slayerTypes.find(
+        (st) => st.name.toLowerCase() === `${monster.name} Slayer`.toLowerCase()
+      )
       if (monsterSlayerType) {
-        console.log(`monster: ${monster.id} slayerType: ${monsterSlayerType.name}`)
         matchingSlayerTypes.push(monsterSlayerType)
       }
       // now look for a super slayer type and add that as well
       const slayerTypeNames = monster.hoverStats.match(/Slayer: ([A-Za-z, ]+)/g)
       if (slayerTypeNames && slayerTypeNames.length > 0) {
         const extractedSlayerTypeNames = slayerTypeNames[0].replace('Slayer: ', '').split(',')
-        console.log(`monster: ${monster.id} extractedSlayerTypeNames: ${extractedSlayerTypeNames}`)
         for (const slayerTypeName of extractedSlayerTypeNames) {
-          const slayerType = slayerTypes.find((st) => st.name === `${slayerTypeName.trim()} (Super Slayer)`)
+          const slayerType = slayerTypes.find(
+            (st) =>
+              st.name.toLowerCase() === `${slayerTypeName.trim()} (Super Slayer)`.toLowerCase() ||
+              st.name.toLowerCase() === `${slayerTypeName.trim()} Slayer`.toLowerCase()
+          )
           if (slayerType) {
             matchingSlayerTypes.push(slayerType)
           }
         }
       }
       //now add the slayer types to the monster_slayers_slayer_type table
+      //only add distinct slayer types
+      matchingSlayerTypes = matchingSlayerTypes.filter(
+        (slayerType, index, self) => index === self.findIndex((t) => t.name === slayerType.name)
+      )
       for (const slayerType of matchingSlayerTypes) {
         await queryRunner.query(
           `INSERT INTO "monster_slayers_slayer_type"("monsterId", "slayerTypeId") VALUES (${monster.id}, ${slayerType.id})`
